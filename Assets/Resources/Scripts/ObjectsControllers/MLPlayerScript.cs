0379cca6d339c54538c62116b00924715da4f0f0
josayarh <johakim.sayarh@gmail.com>
2020-02-23 16:17:14 +0100

    small fixes to player

---
+++ Assets/Resources/Scripts/ObjectsControllers/MLPlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLPlayerScript : MonoBehaviour
+{
+    private static MLPlayerScript playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playerScript == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            playerScript = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
